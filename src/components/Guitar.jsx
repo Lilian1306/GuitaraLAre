10bf3365dbce23a9083c6c5b4c8a3406cd19f836
DaniM0613 <wwh.rj.92@gmail.com>
2025-02-19 22:42:56 -0600

    Agregando guitarras al carrito

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,9 +1,8 @@
 
 
-export default function Guitar({guitar}) {
+export default function Guitar({guitar, addToCart}) {
 
     const {id, name, price, description, image} = guitar
-
  
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -17,6 +16,7 @@ export default function Guitar({guitar}) {
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
+                        onClick={() => addToCart(guitar)}
                     >Agregar al Carrito</button>
                 </div>
             </div>
