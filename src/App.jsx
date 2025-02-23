92d09224fe67bcd2d5f7ff136c160b74581c4831
DaniM0613 <wwh.rj.92@gmail.com>
2025-02-22 19:18:05 -0600

    funcion para decrementar el producto en el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   const [ data, setData] = useState(db) 
   const [cart, setCart ] = useState([]) 
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 0
 
 
   // Funcion para agregar guitarras al carrito
@@ -30,11 +32,41 @@ function App() {
     setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
   }
 
+  // Funcion para incrementar el producto en el carrito
+  function increaseQuantity (id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS ) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  // Funcion para decrementar el producto en el carrito 
+  function decrementQuantity (id) {
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS){
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
   return (
     <>
     <Header
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
     />
 
     <main className="container-xl mt-5">
